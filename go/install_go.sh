b43efaf50c7b381b775f8ab0d15c0100d32bcc18
metairie <metairie@ub20.04>
2021-05-25 10:48:58 +0200

    download latest version of Go

--- go/install_go.sh
+++ go/install_go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://golang.org/dl/go1.16.3.linux-amd64.tar.gz
+wget "https://dl.google.com/go/$(curl https://golang.org/VERSION?m=text).linux-amd64.tar.gz"
 rm -rf /usr/local/go
 tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz
 ln -s /usr/local/go/bin/go /usr/bin/go
